dd0236df73350b1173a8c34b1fc3a917c806e495
ack2406 <ackmp@pm.me>
2022-05-28 12:41:04 +0200

    Add basic structure

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.endswith('.'):
+        response = 'celnie.'
+        await message.channel.send(response)
+        return
+
+
+client.run(TOKEN)
